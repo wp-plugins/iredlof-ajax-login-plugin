ca68031b21b86dc6257a18dac7411ef0041d382c
rlchandani <rlchandani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-20 17:46:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/iredlof-ajax-login-plugin/trunk@127838 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iredlof-ajax-login-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: iRedlof Ajax Login
+Plugin URI: http://iredlof.com/2009/04/iredlof-ajax-login-wordpress-plugin/
+Description: iRedlof Ajax Login adds a Top Panel to let users login using AJAX Technology. This plugin is based on my iRedlof Inspire Theme. Checkout the toppanel demo on <a href='http://rlchandani.co.cc'>http://rlchandani.co.cc</a>. To use the plugin add the following line just after the &lt;body&gt; tag in header.php file of your current theme <strong>&lt;?php if(function_exists(updateHeader)) updateHeader(); ?&gt;</strong> .
+Author: Rohit LalChandani
+Version: 2.2
+Author URI: http://iredlof.com
+*/
+
+require( dirname(__FILE__) . '/update-content.php' );
+load_plugin_textdomain('iRLogin','wp-content/plugins/iredlof-ajax-login-plugin/');
+
+function string_limit_words($string, $word_limit)
+{
+  $words = explode(' ', $string, ($word_limit + 1));
+  if(count($words) > $word_limit) {
+  array_pop($words);
+  //add a ... at last article when more than limit word count
+  echo implode(' ', $words)."..."; } else {
+  //otherwise
+  echo implode(' ', $words); }
+}
+
+wp_enqueue_script('jquery');
+wp_enqueue_script('jquery-form');
+wp_enqueue_script('iRedlof-jquery', "/".PLUGINDIR.'/'.dirname(plugin_basename(__FILE__)).'/js/jquery-1.3.2.min.js', array('jquery', 'jquery-form'));
+wp_enqueue_script('iRedlof-slide', "/".PLUGINDIR.'/'.dirname(plugin_basename(__FILE__)).'/js/slide.js', array('jquery', 'jquery-form'));
+wp_enqueue_script('iRedlof-tabcontent', "/".PLUGINDIR.'/'.dirname(plugin_basename(__FILE__)).'/js/tabcontent.js', array('jquery', 'jquery-form'));
+?>
